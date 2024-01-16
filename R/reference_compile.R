8387659854b281f58abccd692d4fbbff5ed000ef
Tyler Scott <tascott@ucdavis.edu>
2024-01-16 15:22:09 -0800

    swap from pblapply to mclapply

--- R/reference_compile.R
+++ R/reference_compile.R
@@ -3,23 +3,27 @@
 #' Reads in JSON files of extracted references and combines them inot tabular data
 #'
 #' @param ref_dir directory name that holds the file(s) with .JSON extension extracted by Anystyle.io
-#' @param cores how many cores you want to use in pblapply, defaults to 1
+#' @param cores how many cores you want to use in mclapply, defaults to 1
 #' @return data table
 #'
 #' @examples dt <- reference_compile('reference_extracts_gsp')
 #' @import data.table
 #' @import magrittr
-#' @importFrom pbapply pblapply
+#' @importFrom parallel mclapply
 #' @importFrom jsonlite fromJSON
 #' @export
 
 reference_compile <- function(ref_dir,cores = 1){
   fl = list.files(ref_dir, full.names = T, pattern = 'json', recursive = T)
-  fl_list = pblapply(fl,function(x) fromJSON(x) %>% data.table() %>% .[,File:=x],cl = cores)
-  retry = which(sapply(fl_list,class)=='try-error')
-  replace_entries = pblapply(fl[retry],function(x) fromJSON(x) %>%
-                                 data.table() %>% .[,File:=basename(x)],cl = cores)
-  fl_list[retry] <- replace_entries
+  l = length(fl)
+  if(l > 100){print(paste('compiling',l,'files, this will not be instantaneous'))}
+  fl_list = mclapply(fl,function(x) fromJSON(x) %>% data.table() %>% .[,File:=x],mc.cores = cores,mc.preschedule = T,mc.cleanup = T)
+  if(any(sapply(fl_list,class)=='try-error')){
+    retry = which(sapply(fl_list,class)=='try-error')
+    replace_entries = mclapply(fl[retry],function(x) fromJSON(x) %>%
+                                 data.table() %>% .[,File:=basename(x)],mc.cores = cores/2,mc.preschedule = T,mc.cleanup = T)
+    fl_list[retry] <- replace_entries
+  }
   fl_dt = rbindlist(fl_list,fill = T,use.names=T)
 
   dt = data.table()
